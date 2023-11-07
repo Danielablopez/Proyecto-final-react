446a68c015e4558c8edc542d6e5536d7a7db08d6
Daniela Betiana Lopez <135769119+Danielablopez@users.noreply.github.com>
2023-11-06 22:42:50 -0300

    primer comit

---
+++ src/Portada.jsx
@@ -0,0 +1,21 @@
+// import * as React from "react";
+import { Link } from "react-router-dom";
+import Form from "./Componentes/Form";
+
+function Portada () {
+    
+    return (
+        <>
+            <br /><h1>Seguros del Hogar 🏬</h1><br />
+            <Form/>
+             <br /> <br /> <Link to={"historial"}>
+             <button id="historial" className="btn-dark">
+                    <span>Ver historial </span>
+                </button>
+            </Link>
+            
+        </>
+    )
+}
+
+export default Portada
